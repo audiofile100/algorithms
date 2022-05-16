02260cc0c0102f96eceafda147635564f18233d2
audiofile100 <amplified100@gmail.com>
2022-05-16 12:13:08 -0600

    dfs

---
+++ src/main/java/graphs/DFS.java
@@ -0,0 +1,20 @@
+package graphs;
+
+import adt.Graph;
+
+public class DFS {
+
+    private final boolean[] visited;
+
+    public DFS(int vertices) {
+        visited = new boolean[vertices];
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public void dfsIterative(Graph graph) {
+        
+    }
+}
