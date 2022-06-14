67af343bc3ca6b0ab79880a3ba25e620d532ced8
audiofile100 <amplified100@gmail.com>
2022-06-13 21:43:11 -0600

    redesigned graphs

---
+++ src/main/java/algoilluminated/graph/ds/DiGraph.java
@@ -0,0 +1,45 @@
+package algoilluminated.graph.ds;
+
+import lombok.Builder;
+
+import java.util.*;
+
+/**
+ * Unweighted graph with directed egdes.
+ */
+public class DiGraph {
+    @Builder
+    private static class Edge {
+        String src;
+        String dest;
+        int weight;
+    }
+    @Builder
+    private static class Node {
+        String key;
+        List<Edge> outgoing;
+    }
+
+    private final Map<String, Node> map;
+
+    public DiGraph(String[] vertices) {
+        map = new HashMap<>();
+        Arrays.stream(vertices).forEach(v -> map.put(v, Node.builder().key(v).outgoing(new ArrayList<>()).build()));
+    }
+
+    public void addEdge(String src, String dest, int weight) {
+        Edge edge = Edge.builder().src(src).dest(dest).weight(weight).build();
+        map.get(src).outgoing.add(edge);
+    }
+
+    // --- print weighted graph
+    public void print() {
+        for (Node vertex : map.values()) {
+            System.out.print(vertex.key + ": ");
+            for (Edge e : vertex.outgoing) {
+                System.out.print("(" + e.src + "," + e.dest + "," + e.weight + ")");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/main/java/algoilluminated/graph/ds/Graph.java
@@ -0,0 +1,39 @@
+package algoilluminated.graph.ds;
+
+import lombok.Builder;
+
+import java.util.*;
+
+/**
+ * Unweighted graph.
+ */
+public class Graph {
+    @Builder
+    private static class Node {
+        String key;
+        List<Node> neighbors;
+    }
+
+    private final Map<String, Node> map;
+
+    public Graph(String[] vertices) {
+        map = new HashMap<>();
+        Arrays.stream(vertices).forEach(v -> map.put(v, Node.builder().key(v).neighbors(new ArrayList<>()).build()));
+    }
+
+    public void addEdge(String src, String dest) {
+        map.get(src).neighbors.add(map.get(dest));
+        map.get(dest).neighbors.add(map.get(src));
+    }
+
+    // --- print graph
+    public void print() {
+        for (Node vertex : map.values()) {
+            System.out.print(vertex.key + ": ");
+            for (Node n : vertex.neighbors) {
+                System.out.print(n.key + " ");
+            }
+            System.out.println();
+        }
+    }
+}
