0d046894739dc17e5edbb3c0556b69b18ac633c0
audiofile100 <audiofile100@proton.me>
2023-08-18 11:22:01 -0600

    segment min tree

--- src/main/java/tree/SegmentMinTree.java
+++ src/main/java/tree/SegmentMinTree.java
@@ -1,18 +1,20 @@
 package tree;
 
-public class SegmentSumTree {
+public class SegmentMinTree {
 
-    private final int[] tree;
     private final int[] data;
+    private final int[] tree;
+
     private final int begin;
     private final int end;
 
-    public SegmentSumTree(int[] data) {
+    public SegmentMinTree(int[] data) {
         this.data = data;
-        tree = new int[4 * data.length];
 
         begin = 0;
         end = data.length-1;
+
+        tree = new int[4 * data.length];
 
         build(1, begin, end);
     }
@@ -22,26 +24,24 @@ public class SegmentSumTree {
             tree[v] = data[lo];
         } else {
             int mid = lo + (hi - lo) / 2;
-            build( 2*v, lo, mid);
+            build(2*v, lo, mid);
             build(2*v+1, mid+1, hi);
-            tree[v] = tree[2*v] + tree[2*v+1];
+            tree[v] = Math.min(tree[2*v], tree[2*v+1]);
         }
     }
 
-    public int subArraySum(int L, int R) {
-        return subArraySum(1, begin, end, L, R);
+    public int minSubArray(int L, int R) {
+        return minSubArray(1, begin, end, L, R);
     }
 
-    private int subArraySum(int v, int lo, int hi, int L, int R) {
-        if (L > R) {
-            return 0;
-        }
+    private int minSubArray(int v, int lo, int hi, int L, int R) {
+        if (L > R) return Integer.MAX_VALUE;
         if (L == lo && R == hi) {
             return tree[v];
+        } else {
+            int mid = lo + (hi - lo) / 2;
+            return Math.min(minSubArray(2*v, lo, mid, L, Math.min(mid, R)), minSubArray(2*v+1, mid+1, hi, Math.max(L, mid+1), R));
         }
-        int mid = lo + (hi - lo) / 2;
-        return subArraySum(2*v, lo, mid, L, Math.min(R, mid))
-                + subArraySum(2*v+1, mid+1, hi, Math.max(L, mid+1), R);
     }
 
     public void update(int idx, int val) {
@@ -58,26 +58,27 @@ public class SegmentSumTree {
             } else {
                 update(2*v+1, mid+1, hi, idx, val);
             }
-            tree[v] = tree[2*v] + tree[2*v+1];
+            tree[v] = Math.min(tree[2*v], tree[2*v+1]);
         }
     }
 
     public void print() {
-        for (int j : tree) {
-            System.out.print(j + " ");
+        for (int val : tree) {
+            System.out.print(val + " ");
         }
         System.out.println();
     }
 
     public static void main(String[] args) {
 
-        int[] data = { 1, 2, 3, 4, 5 };
+        int[] data = { 1, 8, 3, 4, 5, 6, 7 };
 
-        SegmentSumTree tree = new SegmentSumTree(data);
+        SegmentMinTree tree = new SegmentMinTree(data);
         tree.print();
-        System.out.println(tree.subArraySum(0, 2));
-        tree.update(2, 1);
+        System.out.println(tree.minSubArray(1, 4));
+        tree.update(2, 10);
         tree.print();
-        System.out.println(tree.subArraySum(0, 2));
+        System.out.println(tree.minSubArray(1, 4));
+        System.out.println(tree.minSubArray(0, data.length-1));
     }
 }
