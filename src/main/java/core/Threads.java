6f7ecfdf43548b5929b4ca5d871c2167dc051b3a
audiofile100 <audiofile100@proton.me>
2023-08-19 15:01:31 -0600

    concurrency

---
+++ src/main/java/core/Threads.java
@@ -0,0 +1,15 @@
+package core;
+
+import core.concurrency.AThread;
+
+/**
+ * Created by mg on 8/19/2023.
+ */
+public class Threads {
+
+    public static void main(String[] args) {
+
+//        AThread aThread = new AThread();
+//        aThread.start();
+    }
+}
---
+++ src/main/java/core/concurrency/AThread.java
@@ -0,0 +1,21 @@
+package core.concurrency;
+
+/**
+ * Created by mg on 8/19/2023.
+ */
+public class AThread extends Thread {
+
+//    @Override
+//    public void run() {
+//        long start = System.currentTimeMillis();
+//        int i = 0;
+//        while (true) {
+//            System.out.println(this.getName() + ": New Thread is running..." + i++);
+//            try {
+//                Thread.sleep(3000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+}
