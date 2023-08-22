754e2ae555c715096128eb31df6d49f9ca6a919d
audiofile100 <audiofile100@proton.me>
2023-08-21 21:29:54 -0600

    observer pattern

--- src/main/java/core/design/pattern/behavioral/observer/service/Subject.java
+++ src/main/java/core/design/pattern/behavioral/observer/service/Subject.java
@@ -1,7 +1,5 @@
 package core.design.pattern.behavioral.observer.service;
 
-import core.design.pattern.behavioral.observer.service.Observer;
-
 public interface Subject {
 
     void registerObserver(Observer o);
